d45959e817a983dd01b5db152cba45e4223cdc24
woyman <woyman11@gmail.com>
2025-01-14 23:41:29 +0700

    fix: button control testimoni home

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -316,7 +316,33 @@
               @empty
 
               @endforelse
-                {{--  <div class="relative bg-no-repeat bg-center bg-cover" style="background-image: url('{{ asset('assets/galeri/galery-2.jpeg') }}') ">
+                {{--  
+                <div class="relative bg-no-repeat bg-center bg-cover" style="background-image: url('{{ asset('assets/galeri/galery-2.jpeg') }}') ">
+                    <div
+                        class="opacity-0 hover:opacity-80 bg-white duration-300 absolute inset-0 z-10 hidden md:flex text-white font-semibold items-end p-5">
+                        <div class="flex flex-col">
+
+                            <span class="flex text-black">
+                                <svg class="w-6 h-6 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M13 10a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2H14a1 1 0 0 1-1-1Z"
+                                        clip-rule="evenodd" />
+                                    <path fill-rule="evenodd"
+                                        d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12c0 .556-.227 1.06-.593 1.422A.999.999 0 0 1 20.5 20H4a2.002 2.002 0 0 1-2-2V6Zm6.892 12 3.833-5.356-3.99-4.322a1 1 0 0 0-1.549.097L4 12.879V6h16v9.95l-3.257-3.619a1 1 0 0 0-1.557.088L11.2 18H8.892Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                25 Gambar
+                            </span>
+
+                            <h3 class="text-dark-blue text-xl">
+                                Galeri: Suasana Pembelajaran di SD Muhammadiyah Sapen
+                            </h3>
+                            <p class="italic text-black text-sm"> Oktober 24, 2024</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="relative bg-no-repeat bg-center bg-cover" style="background-image: url('{{ asset('assets/galeri/galery-2.jpeg') }}') ">
                     <div
                         class="opacity-0 hover:opacity-80 bg-white duration-300 absolute inset-0 z-10 hidden md:flex text-white font-semibold items-end p-5">
                         <div class="flex flex-col">
@@ -709,7 +735,7 @@
                         </div>
 
                         <!-- Slider controls -->
-                        <button type="button" class="hidden md:flex absolute top-0 -left-[65px] start-0 z-30 items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" data-carousel-prev>
+                        <button type="button" class="hidden md:flex absolute top-0 -left-[65px] z-30 items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" data-carousel-prev>
                             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                                 <svg class="w-4 h-4 text-dark-blue  rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -717,7 +743,7 @@
                                 <span class="sr-only">Previous</span>
                             </span>
                         </button>
-                        <button type="button" class="hidden md:flex absolute top-0 -right-[65px] end-0 z-30 items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" data-carousel-next>
+                        <button type="button" class="hidden md:flex absolute top-0 -right-[65px] z-30 items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" data-carousel-next>
                             <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                                 <svg class="w-4 h-4 text-dark-blue dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
