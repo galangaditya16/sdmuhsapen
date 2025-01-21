ebdc125b3a6248ebec16d4500b47b737c9fde70b
aditya galang <adityagalang1602@gmail.com>
2025-01-21 15:04:40 +0700

    galery bug tampilan

--- resources/views/frontend/pages/galery.blade.php
+++ resources/views/frontend/pages/galery.blade.php
@@ -5,7 +5,7 @@
 
     <section class="container mx-auto grid grid-cols-1 gap-y-7 md:my-20">
         <div class="md:flex md:justify-between gap-x-10 grid gap-y-5 px-4 md:px-0">
-            <div class="lg:px-0">
+            <div class="lg:px-4">
                 <x-breadcumb :breadcrumbs="[['url' => '', 'name' => 'Galeri Foto Sekolah']]" />
             </div>
 
@@ -22,7 +22,7 @@
                 <div class="flex items-center relative bg-gray-300 rounded-3xl">
                     <input name="search"
                         class="appearance-none bg-transparent border-none rounded-3xl text-center md:text-end font-bold bg-gray-300 w-full text-gray-700 py-2.5 pl-2 pr-10 leading-tight focus:outline-2 focus:border-2"
-                        type="text" placeholder="Cari berita..." aria-label="Cari berita...">
+                        type="text" placeholder="Cari Galeri..." aria-label="Cari Galeri...">
                     <div class="absolute right-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6 text-gray-700">
@@ -34,31 +34,35 @@
             </div>
         </div>
 
-        <div class="px-4 lg:px-0 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div class="px-4 lg:px-4 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             @forelse ($galeries as $new)
                 <div style="background-image: url('{{ asset('assets/images/gallery/thumbnail/').'/'. $new->thumbnail }}');" alt="galeries-1.png"
-                    class="rounded-3xl bg-cover bg-center h-[565px] max-h-[600px] w-full relative overflow-hidden">
+                    class="rounded-3xl bg-cover bg-center h-[450px] max-h-[500px] w-full shadow-xl hover:-translate-y-1 hover:scale-101 duration-150 relative overflow-hidden">
                     <div class="h-[565px] relative">
                         <div
-                            class="py-1.5 px-4 bg-black bg-opacity-40 text-white rounded-lg absolute left-8 top-[235px] flex gap-x-1">
+                            class="py-1.5 px-4 bg-black bg-opacity-40 text-white rounded-lg absolute left-8 top-[180px] flex gap-x-1">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
-                            <p>25 Foto</p>
+                            <p>{{ $new->countGallery() ?? '0' }} Photos</p>
                         </div>
                     </div>
                     <div class="absolute bottom-0 bg-white pt-6 px-9 h-1/2 w-full">
                         <div class="space-y-3">
                             @if ($lang == 'id')
+                                <a href="{{ route('galeryDetail', $new->slug_id) }}">
                                 <p class="text-lg font-bold">Galeri: {{ $new->title_id }}</p>
+                                </a>
                             @else
+                                <a href="{{ route('galeryDetail', $new->slug_en) }}">
                                 <p class="text-lg font-bold">Galeri: {{ $new->title_en }}</p>
+                                </a>
                             @endif
                             <p class="font-thin text-sm">{{ $new->created_at }}</p>
                         </div>
-                        <div class="flex justify-center mt-5">
+                        <div class="flex justify-center mt-20">
                             @if ($lang == 'id')
                                 <a href="{{ route('galeryDetail', $new->slug_id) }}"
                                     class="py-2 px-10 bg-oren text-white rounded-3xl">Lihat Galeri</a>
